e308567e2b59b3758371e2924d268493cbfbca6c
kalpana <kalyanikuttykalpana152@gmail.com>
2026-07-06 12:26:11 +0530

    Add files via upload

---
+++ day5/EnhancedForLoop.java
@@ -0,0 +1,10 @@
+package day5;
+
+public class EnhancedForLoop {
+    public static void main(String[] args){
+        String[] arr=new String[]{"Kalpana","390AEROlR908"};
+        for(String num:arr){
+            System.out.print(num+" ");
+        }
+    }
+}
