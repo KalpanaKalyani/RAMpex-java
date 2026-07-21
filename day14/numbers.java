660846228f0e0d4a57fc491fbda1b8f7dcf421b7
kalpana <kalyanikuttykalpana152@gmail.com>
2026-07-21 20:00:24 +0530

    Add files via upload

---
+++ day14/numbers.java
@@ -0,0 +1,14 @@
+package day14;
+
+public class numbers {
+    public static void main(String[] args){
+       print(1);
+    }
+     static void print(int n){
+        if(n>10){
+            return;
+        }
+         System.out.println(n);
+        print(n+1);
+    }
+}
